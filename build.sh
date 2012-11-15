3c540506b86cd39f15c09beab9208ce438629589
Kerby Smith <kerby@Romulus.(none)>
2012-11-14 19:43:35 -0700

    update build script and fix keyboard issue

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+currentdir=`pwd`
 pushd ~/libraries/python-for-android/dist/default/
 rm -rf ./bin/*
-./build.py --package org.test.particlepanda --name ParticlePanda --version 0.5 --dir '/media/hd/Documents/Chaos Buffalo/CBLParticleSystem/' debug
+./build.py --package org.test.particlepanda --name ParticlePanda --version 0.5 --dir "$currentdir" debug
 popd
 cp ~/libraries/python-for-android/dist/default/bin/ParticlePanda-0.5-debug.apk ./
